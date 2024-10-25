2fbdb1fd1a5dc32e00e58f9c844028231bc512f0
dcardenas-robert <drcpcardenas@gmail.com>
2024-10-24 21:56:37 -0600

    Se protegio la ruta de crear una categoria mediante el uso de un token que el usuario debe mandar en cada peticion pot lo headers

--- routes/categories.router.js
+++ routes/categories.router.js
@@ -3,6 +3,7 @@ const express = require('express');
 const CategoryService = require('./../services/category.service');
 const validatorHandler = require('./../middlewares/validator.handler');
 const { createCategorySchema, updateCategorySchema, getCategorySchema } = require('./../schemas/category.schema');
+const passport = require('passport');
 
 const router = express.Router();
 const service = new CategoryService();
@@ -30,6 +31,7 @@ router.get('/:id',
 );
 
 router.post('/',
+  passport.authenticate('jwt', { session: false }),
   validatorHandler(createCategorySchema, 'body'),
   async (req, res, next) => {
     try {
